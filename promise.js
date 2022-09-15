c804808fb10e3fa23f4bf94805a9fe1825f2a711
Hajar Aswad <hajaraswadkom@gmail.com>
2022-09-15 23:06:17 +0700

    promise passed

--- promise.js
+++ promise.js
@@ -1,7 +1,27 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (param) => {
+  let count = 0;
+  const result1 = await promiseTheaterIXX(param)
+  const result2 = await promiseTheaterVGC(param)
+
+  for (let i = 0; i < result1.length; i++) {
+    if (param === result1[i].hasil) {
+      count++
+    }
+  }
+
+  for (let i = 0; i < result2.length; i++) {
+    if (param === result2[i].hasil) {
+      count++
+    }
+  }
+
+  return count;
+
+
+}
 
 module.exports = {
   promiseOutput,
